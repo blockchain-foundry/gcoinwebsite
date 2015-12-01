057af605d7c4651e4607723d2376b630ef286313
solomon wu <wusolomon1@gmail.com>
2015-12-01 20:06:10 +0800

    gcoin.com.tw initial commit

---
+++ gcoinwebsite/src/tw/com/gcoin/website/GcoinwebsiteServlet.java
@@ -0,0 +1,13 @@
+package tw.com.gcoin.website;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class GcoinwebsiteServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
---
+++ gcoinwebsite/war/static/js/common.js
@@ -0,0 +1,49 @@
+$('body').removeClass('no-js');
+
+// Close the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+	$('.navbar-toggle:visible').click();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Add the dropdown menu button */
+$('.navbar-brand').after('<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse"> 		<i class="fa fa-bars"></i> 	</button> ');
+
+$(document).ready(function(){
+	var set_navbar_bg = function() {
+		var scroll = $(this).scrollTop();
+		scroll = (Math.min(300, scroll) / 300);
+		if (open_navbar_menu) {
+			scroll = 1;
+		}
+		$('nav').css({"background-color":"rgba(51, 51, 102, "+scroll+")"});
+		$('.current').css({"background-color":"rgba(215, 230, 255, "+0.3*scroll+")"});
+	}
+
+	var open_navbar_menu = false;
+	$('.navbar-toggle').click(function() {
+		open_navbar_menu = !open_navbar_menu;
+		if (open_navbar_menu) {
+			$('nav').css({"transition":"background-color 0.3s"});
+		} else {
+			window.setTimeout(function(){
+				$('nav').css({"transition":"none"});
+			}, 300);
+		}
+		set_navbar_bg();
+	});
+
+	set_navbar_bg();
+	$(document).scroll(set_navbar_bg);
+});
